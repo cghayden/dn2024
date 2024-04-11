055fe91de2e693893661608604259a24794f7b0d
Corey Hayden <cghayden@gmail.com>
2024-04-10 21:43:53 -0400

    show selected parent dance in modal on small screens

--- app/routes/parent.allClasses.$danceClassId.tsx
+++ app/routes/parent.allClasses.$danceClassId.tsx
@@ -1,6 +1,5 @@
 import { LoaderFunctionArgs } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { useEffect } from "react";
+import { useLoaderData, useNavigate } from "@remix-run/react";
 import { ContentContainer } from "~/components/styledComponents/ContentContainer";
 import { getDanceClass, requireParentUserId } from "~/models/parent.server";
 
@@ -28,10 +27,16 @@ export default function SelectedDanceClass() {
     studio,
     studioNotes,
   } = useLoaderData<typeof loader>();
-
+  const navigate = useNavigate();
   return (
-    <div className="px-4">
-      <ContentContainer className="p-4">
+    <div className="modalOnSmallScreen px-4">
+      <button
+        className="closeModal  mb-4 mt-4 rounded bg-blue-500 px-4 py-2 text-white"
+        onClick={() => navigate(-1)}
+      >
+        Back
+      </button>
+      <ContentContainer className="modalContainer p-4">
         <h2 className="pb-4 text-center text-lg font-bold">{name}</h2>
         <div className="">
           {performanceName && <p>"{performanceName}"</p>}
