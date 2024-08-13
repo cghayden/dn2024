bd987934e89a07775865c34a2686178aba861ee0
Corey Hayden <cghayden@gmail.com>
2024-08-13 11:56:45 -0400

    active studio search with debounce

--- app/routes/parent.searchStudios.tsx
+++ app/routes/parent.searchStudios.tsx
@@ -1,77 +1,99 @@
-import { useForm } from '@conform-to/react'
-import { Form, Link, useActionData } from '@remix-run/react'
-import { TextInput } from '~/components/forms/TextInput'
-import { ContentContainer } from '~/components/styledComponents/ContentContainer'
-import { z } from 'zod'
-import { json, type ActionFunctionArgs } from '@remix-run/node'
-import { parse } from '@conform-to/zod'
-import { parentSearchStudios } from '~/models/studio.server'
-import { PageHeader } from '~/components/styledComponents/PageHeader'
+import { useForm } from "@conform-to/react";
+import { Form, Link, useActionData, useSubmit } from "@remix-run/react";
+import { TextInput } from "~/components/forms/TextInput";
+import { ContentContainer } from "~/components/styledComponents/ContentContainer";
+import { z } from "zod";
+import { json, type ActionFunctionArgs } from "@remix-run/node";
+import { parse } from "@conform-to/zod";
+import { parentSearchStudios } from "~/models/studio.server";
+import { PageHeader } from "~/components/styledComponents/PageHeader";
+import { useEffect, useState } from "react";
 
 const searchSchema = z.object({
   searchVal: z.string(),
-})
+});
 
 export async function action({ request }: ActionFunctionArgs) {
-  const formData = await request.formData()
-  const submission = parse(formData, { schema: searchSchema })
+  const formData = await request.formData();
+  const submission = parse(formData, { schema: searchSchema });
 
   if (!submission.value) {
-    return json({ status: 'error', submission, studios: [] })
+    return json({ status: "error", submission, studios: [] });
   }
 
-  const { searchVal } = submission.value
+  const { searchVal } = submission.value;
 
-  const studios = await parentSearchStudios({ searchVal })
+  const studios = await parentSearchStudios({ searchVal });
 
-  return json({ submission, studios })
+  return json({ submission, studios });
 }
 
 export default function SearchStudios() {
-  const actionData = useActionData<typeof action>()
+  const actionData = useActionData<typeof action>();
+  console.log("actionData", actionData);
+  const submit = useSubmit();
 
   const [form, { searchVal }] = useForm({
     lastSubmission: actionData?.submission,
-  })
+  });
+  const [inputValue, setInputValue] = useState("");
+  const [debouncedInputValue, setDebouncedInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      console.log("do something");
+      const formData = new FormData();
+      formData.append("searchVal", inputValue);
+      submit(formData, { method: "post" });
+      submit;
+      setDebouncedInputValue(inputValue);
+    }, 500);
+    return () => clearTimeout(timeoutId);
+  }, [inputValue]);
 
   return (
     <div>
       <PageHeader
-        headerText='Search For A Studio'
-        navigateBackUrl={'/parent'}
+        headerText="Search For A Studio"
+        navigateBackUrl={"/parent"}
       />
-      <ContentContainer className='w-5/6 max-w-[600px]'>
-        <Form {...form.props} method='post' className='form_default w-5/6'>
-          <div className='input_section wrapper'>
-            <div className='input_item'>
+      <ContentContainer className="w-5/6 max-w-[600px]">
+        <Form {...form.props} method="post" className="form_default w-5/6">
+          <div className="input_section wrapper">
+            <div className="input_item">
               <TextInput
-                label={'Search for a Studio'}
-                name='searchVal'
+                label={"Search for a Studio"}
+                name="searchVal"
                 error={searchVal.error}
                 required={true}
+                onChange={handleInputChange}
               />
             </div>
           </div>
-          <div className='input-item mt-4'>
-            <button type='submit' className='btn btn-action'>
+          <div className="input-item mt-4">
+            <button type="submit" className="btn btn-action">
               Search
             </button>
           </div>
         </Form>
       </ContentContainer>
       <div>
-        <h2 className='p-6 '>
+        <h2 className="p-6 ">
           Search Results... Choose a studio to browse dance classes
         </h2>
         <ul>
           {actionData?.studios.map((studio) => (
-            <li key={studio.userId} className='m-2 w-4/5 mx-auto'>
-              <ContentContainer className='bg-gray-200'>
+            <li key={studio.userId} className="m-2 mx-auto w-4/5">
+              <ContentContainer className="bg-gray-200">
                 <Link
                   to={`/parent/browseStudioDances/${studio.userId}`}
-                  className='inline-block w-full'
+                  className="inline-block w-full"
                 >
-                  <div className='p-4 flex justify-evenly'>
+                  <div className="flex justify-evenly p-4">
                     <p>{studio.name}</p>
                     <p>Address...</p>
                   </div>
@@ -82,5 +104,5 @@ export default function SearchStudios() {
         </ul>
       </div>
     </div>
-  )
+  );
 }
